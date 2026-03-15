40493694e0c75ceaffc90e3b78d87b4a223262eb
shunsuke5 <shunsuke1359@gmail.com>
2026-03-15 18:49:56 +0900

    カレンダー初期表示 完了

--- calendar.js
+++ calendar.js
@@ -1,13 +1,23 @@
-const now = new Date();
-const nowYear = now.getFullYear();
-const nowMonth = now.getMonth();
-const hiddenYear = document.getElementById("year");
-hiddenYear.value = nowYear;
-const table = document.querySelector(".calendar");
-const tbodyList = table.querySelectorAll("tbody");
-tbodyList.forEach(tbody => {
-    const tdList = tbody.querySelectorAll("td");
-    tdList.forEach(td => {
-    
-    });   
-});
+function initCalendar() {
+    const target = new Date();
+    const nowMonth = target.getMonth();
+    target.setDate(1);
+    document.getElementById("year").value = target.getFullYear();
+    const table = document.querySelector(".calendar");
+    const tbodyList = table.querySelectorAll("tbody");
+    tbodyList.forEach(tbody => {
+        const tdList = tbody.querySelectorAll("td");
+        tdList.forEach((td, idx) => {
+            if (target.getMonth() !== nowMonth) {
+                return;
+            }
+
+            if (target.getDay() === idx) {
+                td.textContent = target.getDate();
+                target.setDate(target.getDate() + 1);
+            }
+        });   
+    });
+}
+
+initCalendar();
